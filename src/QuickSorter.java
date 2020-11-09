72b81afd3155a1a6ca3bb8b26c05cbf6e438d48a
Ben <benh@genesis-analytics.com>
2020-11-09 10:20:56 +0000

    Initial commit

---
+++ src/QuickSorter.java
@@ -0,0 +1,55 @@
+
+public class QuickSorter {
+    int [] list;
+
+
+    public void quickSort(int [] arr, int low, int high) {
+        if (low < high)
+        {
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before
+            // partition and after partition
+            quickSort(arr, low, pi-1);
+            quickSort(arr, pi+1, high);
+        }
+    }
+
+    /* Takes last element as pivot,
+       places the pivot element at its correct
+       position, and places all
+       smaller (value-wise) to left of
+       pivot and all greater elements to right
+     */
+    int partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++)
+        {
+            // If current element is smaller than the pivot
+            if (arr[j] < pivot)
+            {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = {1,5,};
+    }
+}
